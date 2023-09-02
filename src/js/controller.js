b7264f103639179df5eaf2ea47ac6eb010c44f65
Igor Stepanov <stipyk1309@gmail.com>
2023-09-02 20:52:26 +0300

    config and helper files

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,13 +4,7 @@ import recipeView from './views/recipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-// const timeout = function (s) {
-//   return new Promise(function (_, reject) {
-//     setTimeout(function () {
-//       reject(new Error(`Request took too long! Timeout after ${s} second`));
-//     }, s * 1000);
-//   });
-// };
+
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -27,7 +21,7 @@ const controlRecipes = async function () {
     //2. Rendering the recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
 
