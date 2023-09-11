60450ff1367dc426555ad15d35d8526fca5f625a
Igor Stepanov <stipyk1309@gmail.com>
2023-09-11 21:11:57 +0300

    recipes search functional implement

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,15 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-
-
-// https://forkify-api.herokuapp.com/v2
+//hot reload (coming from parcel):
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -25,8 +28,26 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    //1. Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //2. Load search results
+    await model.loadSearchResults(query);
+
+    //3. Render search results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
