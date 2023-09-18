69812acb49bfe6d669b15a65db54654f14eff47f
Igor Stepanov <stipyk1309@gmail.com>
2023-09-18 16:57:30 +0300

    DOM updating algorithm

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -15,9 +15,9 @@ class RecipeView extends View {
     this._parentElement.addEventListener('click', e => {
       const btn = e.target.closest('.btn--update-servings');
       if (!btn) return;
-      const { updateTo } = +btn.dataset;
+      const updateTo = +btn.dataset.updateTo;
 
-      if (+updateTo > 0) handler(+updateTo);
+      if (updateTo > 0) handler(updateTo);
     });
   }
 
